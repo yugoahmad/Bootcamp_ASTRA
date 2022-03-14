fb08cc08547a5ea56f0d8d61590e90736b3229ce
yugo <yugoahmadabdillah07@gmail.com>
2022-03-14 16:30:03 +0700

    initial project Yugo

---
+++ Fundamental/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//call other class
+
+day01.ConsoleIntro.MultiConsole();
+
+Console.ReadLine();
---
+++ Fundamental/day01/ConsoleIntro.cs
@@ -0,0 +1,24 @@
+﻿namespace day01
+{
+    class ConsoleIntro
+    {
+        public static void MultiConsole()
+        {
+            //concat
+            Console.WriteLine("Hello, World!" + " " + "Yugo Ganteng banget!!!");
+            Console.Write("dotnet ");
+
+            Console.WriteLine("Code id Bootcamp .NET");
+
+            Console.WriteLine("Code Academy \n.NET");
+
+            //formating number
+            Console.WriteLine("Tanggal {0} suhu {1} jarak {2}", DateTime.Today, 12.0, 78);
+
+            //verbatim
+            Console.WriteLine(@"I Love
+Programming in
+Code id");
+        }
+    }
+}
